047b4eb748d9bc550bc5b2b578c901a5e2c8cc57
monisha2608 <monishakumari287@gmail.com>
2025-10-31 00:57:36 -0400

    Add project files.

---
+++ FitLife/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FitLife
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FitLife/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FitLife")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FitLife.Pages")]
